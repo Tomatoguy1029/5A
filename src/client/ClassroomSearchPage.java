8831d317ee064455613787051122708675d04253
tomoya <tomoya.y1622@gmail.com>
2024-05-25 01:08:07 +0900

    'update'

--- src/client/ClassroomSearchPage.java
+++ src/client/ClassroomSearchPage.java
@@ -1,7 +1,6 @@
 package src.client;
 
 import javax.swing.*;
-import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,7 +13,6 @@ import java.util.Calendar;
 public class ClassroomSearchPage extends JFrame {
     private JFrame frame;
     private JPanel filterPanel, sidePanel, timePanel;
-    private DefaultTableModel model;
     private JCheckBox checkBox52, checkBox53, checkBox54, checkBox63;
     private JCheckBox checkBoxPower, checkBoxLargeDesk;
     private JComboBox<String> dayComboBox;
@@ -31,7 +29,6 @@ public class ClassroomSearchPage extends JFrame {
         initializeLabels();
         initializeFilters();
         initializeButtons();
-        initializeTableModel();
         initializeResizeListener();
 
         frame.setVisible(true);
@@ -128,7 +125,7 @@ public class ClassroomSearchPage extends JFrame {
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Search search = new Search();
-                search.showFilteredResults(model, checkBox52, checkBox53, checkBox54, checkBox63, checkBoxPower,
+                search.showFilteredResults(checkBox52, checkBox53, checkBox54, checkBox63, checkBoxPower,
                         checkBoxLargeDesk, dayComboBox, timeCheckBoxes);
             }
         });
@@ -139,7 +136,7 @@ public class ClassroomSearchPage extends JFrame {
         addroomButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Search search = new Search();
-                search.showFilteredResults(model, checkBox52, checkBox53, checkBox54, checkBox63, checkBoxPower,
+                search.showFilteredResults(checkBox52, checkBox53, checkBox54, checkBox63, checkBoxPower,
                         checkBoxLargeDesk, dayComboBox, timeCheckBoxes);
             }
         });
@@ -152,7 +149,7 @@ public class ClassroomSearchPage extends JFrame {
                 String currentTimeSlot = getCurrentTimeSlot();
                 if (!currentTimeSlot.equals("お休み")) {
                     Search search = new Search();
-                    search.showFilteredResultsForTimeSlot(model, checkBox52, checkBox53, checkBox54, checkBox63,
+                    search.showFilteredResultsForTimeSlot(checkBox52, checkBox53, checkBox54, checkBox63,
                             checkBoxPower, checkBoxLargeDesk, currentTimeSlot);
                 } else {
                     JOptionPane.showMessageDialog(frame, "お休みです");
@@ -161,11 +158,7 @@ public class ClassroomSearchPage extends JFrame {
         });
     }
 
-    private void initializeTableModel() {// データの初期化 TODO: データをサーバーから取得
-        String[] columnNames = { "Name", "Location", "Seats", "Outlets", "Desk Size", "Available", "Edit" };
-        Object[][] data = getSampleData();
-        model = new DefaultTableModel(data, columnNames);
-    }
+    
 
     private void initializeResizeListener() {// ウィンドウサイズの変更を検知するリスナーの初期化
         frame.addComponentListener(new ComponentAdapter() {
@@ -235,85 +228,6 @@ public class ClassroomSearchPage extends JFrame {
         });
         timer.setInitialDelay(0);
         timer.start();
-    }
-
-    private Object[][] getSampleData() {
-        return new Object[][] {
-                // 52号館
-                { "52-102", 52, 50, 20, 100, "月曜1限,月曜3限,火曜5限,火曜6限,水曜1限,木曜6限,金曜3限", "Edit" },
-                { "52-103", 52, 50, 20, 100, "月曜1限,木曜4限,木曜5限,木曜6限,金曜6限", "Edit" },
-                { "52-104", 52, 50, 20, 100, "月曜5限,火曜3限,火曜5限,水曜5限,木曜1限,木曜4限,木曜5限,金曜1限,金曜2限,金曜5限", "Edit" },
-                { "52-201", 52, 50, 20, 100, "月曜6限,火曜6限,水曜3限,水曜6限,木曜3限,木曜4限,金曜4限,金曜6限", "Edit" },
-                { "52-202", 52, 50, 20, 100, "月曜6限,火曜5限,火曜6限,水曜1限,水曜5限,水曜6限,木曜5限,金曜4限,金曜5限,金曜6限", "Edit" },
-                { "52-204", 52, 50, 20, 100, "月曜2限,月曜5限,月曜6限,火曜1限,火曜5限,火曜6限,水曜6限,木曜1限,木曜5限,木曜6限,金曜4限,金曜5限,金曜6限",
-                        "Edit" },
-                { "52-302", 52, 50, 20, 100, "月曜5限,月曜6限,火曜5限,火曜6限,水曜4限,水曜5限,水曜6限,木曜1限,木曜4限,木曜5限,木曜6限,金曜6限", "Edit" },
-                { "52-303", 52, 50, 20, 100, "月曜1限,月曜3限,月曜6限,火曜5限,火曜6限,水曜1限,水曜3限,木曜3限,木曜4限,木曜5限,木曜6限,金曜2限,金曜6限",
-                        "Edit" },
-                { "52-304", 52, 50, 20, 100, "月曜1限,月曜5限,月曜6限,火曜1限,火曜5限,火曜6限,水曜1限,木曜3限,木曜4限,木曜5限,木曜6限", "Edit" },
-                // 53号館
-                { "53-B01", 53, 0, 0, 0, "月曜6限,火曜5限,火曜6限,水曜6限,木曜5限,木曜6限,金曜6限", "Edit" },
-                { "53-B03", 53, 0, 0, 0, "月曜6限,火曜5限,火曜6限,水曜6限,木曜1限,木曜2限,木曜6限,金曜6限", "Edit" },
-                { "53-B04", 53, 0, 0, 0, "月曜3限,月曜6限,火曜5限,火曜6限,水曜5限,水曜6限,木曜1限,木曜3限,木曜4限,木曜5限,木曜6限,金曜4限,金曜5限,金曜6限",
-                        "Edit" },
-                { "53-101", 53, 0, 0, 0, "月曜3限,月曜6限,火曜6限,水曜4限,木曜2限,金曜5限,金曜6限", "Edit" },
-                { "53-103", 53, 0, 0, 0, "月曜1限,月曜3限,月曜6限,火曜4限,火曜6限,水曜6限,木曜6限,金曜4限,金曜6限", "Edit" },
-                { "53-104", 53, 0, 0, 0, "月曜6限,火曜2限,火曜6限,水曜4限,水曜6限,木曜3限,木曜4限,木曜5限,木曜6限,金曜4限,金曜5限,金曜6限", "Edit" },
-                { "53-201", 53, 0, 0, 0, "月曜1限,月曜6限,火曜6限,水曜5限,木曜4限,木曜6限,金曜6限", "Edit" },
-                { "53-203", 53, 0, 0, 0, "月曜1限,月曜5限,月曜6限,火曜6限,水曜1限,水曜6限,金曜4限,金曜6限", "Edit" },
-                { "53-204", 53, 0, 0, 0, "月曜1限,月曜6限,水曜6限,木曜6限,金曜5限,金曜6限", "Edit" },
-                { "53-301", 53, 0, 0, 0, "月曜1限,月曜6限,火曜1限,火曜6限,水曜1限,水曜6限,木曜4限,木曜6限,金曜6限", "Edit" },
-                { "53-303", 53, 0, 0, 0, "月曜1限,火曜6限,水曜6限,木曜4限,木曜5限,木曜6限,金曜6限", "Edit" },
-                { "53-304", 53, 0, 0, 0, "月曜6限,火曜3限,火曜6限,水曜6限,木曜4限,金曜3限,金曜4限,金曜5限,金曜6限", "Edit" },
-                { "53-401", 53, 0, 0, 0, "月曜1限,月曜6限,火曜1限,火曜6限,水曜1限,水曜6限,木曜6限,金曜4限,金曜5限,金曜6限", "Edit" },
-                { "53-403", 53, 0, 0, 0, "月曜1限,月曜6限,火曜4限,火曜6限,水曜1限,木曜4限,木曜5限,木曜6限,金曜6限", "Edit" },
-                { "53-404", 53, 0, 0, 0,
-                        "月曜1限,月曜4限,月曜6限,火曜4限,火曜5限,火曜6限,水曜1限,水曜5限,水曜6限,木曜1限,木曜4限,木曜6限,金曜3限,金曜4限,金曜5限,金曜6限", "Edit" },
-                // 54号館
-                { "54-B01", 54, 0, 0, 0, "月曜2限,月曜3限", "Edit" },
-                { "54-B02", 54, 0, 0, 0, "月曜2限,月曜3限,火曜4限", "Edit" },
-                { "54-B03", 54, 0, 0, 0, "火曜1限,火曜2限", "Edit" },
-                { "54-B04", 54, 0, 0, 0, "水曜3限,水曜4限", "Edit" },
-                { "54-101", 54, 0, 0, 0, "月曜2限,月曜3限", "Edit" },
-                { "54-102", 54, 0, 0, 0, "月曜2限,月曜3限,火曜4限", "Edit" },
-                { "54-103", 54, 0, 0, 0, "火曜1限,火曜2限", "Edit" },
-                { "54-104", 54, 0, 0, 0, "水曜3限,水曜4限", "Edit" },
-                { "54-201", 54, 0, 0, 0, "月曜2限,月曜3限", "Edit" },
-                { "54-202", 54, 0, 0, 0, "月曜2限,月曜3限,火曜4限", "Edit" },
-                { "54-203", 54, 0, 0, 0, "火曜1限,火曜2限", "Edit" },
-                { "54-204", 54, 0, 0, 0, "水曜3限,水曜4限", "Edit" },
-                { "54-301", 54, 0, 0, 0, "月曜2限,月曜3限", "Edit" },
-                { "54-302", 54, 0, 0, 0, "月曜2限,月曜3限,火曜4限", "Edit" },
-                { "54-303", 54, 0, 0, 0, "火曜1限,火曜2限", "Edit" },
-                { "54-304", 54, 0, 0, 0, "水曜3限,水曜4限", "Edit" },
-                { "54-401", 54, 0, 0, 0, "月曜2限,月曜3限", "Edit" },
-                { "54-402", 54, 0, 0, 0, "月曜2限,月曜3限,火曜4限", "Edit" },
-                { "54-403", 54, 0, 0, 0, "火曜1限,火曜2限", "Edit" },
-                { "54-404", 54, 0, 0, 0, "水曜3限,水曜4限", "Edit" },
-                // 63号館
-                { "PCルームA", 63, 0, 0, 0,
-                        "月曜3限,月曜4限,月曜5限,月曜6限,月曜7限,火曜3限,火曜5限,火曜6限,火曜7限,水曜2限,水曜3限,水曜4限,水曜6限,水曜7限,木曜2限,木曜3限,木曜4限,木曜5限,木曜6限,木曜7限,金曜5限,金曜6限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームB", 63, 0, 0, 0,
-                        "月曜5限,月曜7限,火曜3限,火曜4限,火曜5限,火曜6限,火曜7限,水曜2限,水曜3限,水曜4限,水曜7限,木曜2限,木曜3限,木曜6限,木曜7限,金曜4限,金曜5限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームC", 63, 0, 0, 0,
-                        "月曜2限,月曜6限,月曜7限,火曜1限,火曜2限,火曜5限,火曜6限,火曜7限,水曜1限,水曜4限,水曜7限,木曜3限,木曜5限,木曜6限,木曜7限,金曜1限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームD", 63, 0, 0, 0,
-                        "月曜1限,月曜5限,月曜6限,月曜7限,火曜1限,火曜2限,火曜3限,火曜4限,火曜5限,火曜6限,火曜7限,水曜2限,水曜3限,水曜4限,水曜5限,水曜6限,水曜7限,木曜1限,木曜2限,木曜3限,木曜4限,木曜5限,木曜6限,木曜7限,金曜3限,金曜4限,金曜5限,金曜6限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームE", 63, 0, 0, 0,
-                        "月曜1限,月曜2限,月曜3限,月曜4限,月曜6限,月曜7限,火曜3限,火曜4限,火曜5限,火曜6限,火曜7限,水曜2限,水曜3限,水曜4限,水曜5限,水曜6限,水曜7限,木曜1限,木曜4限,木曜5限,木曜6限,木曜7限,金曜5限,金曜6限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームF", 63, 0, 0, 0,
-                        "月曜1限,月曜3限,月曜6限,月曜7限,火曜1限,火曜5限,火曜6限,火曜7限,水曜1限,水曜4限,水曜5限,水曜6限,水曜7限,木曜1限,木曜3限,木曜4限,木曜5限,木曜6限,木曜7限,金曜4限,金曜5限,金曜6限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" },
-                { "PCルームG", 63, 0, 0, 0,
-                        "月曜1限,月曜2限,月曜6限,月曜7限,火曜3限,火曜4限,火曜5限,火曜6限,火曜7限,水曜1限,水曜3限,水曜7限,木曜1限,木曜2限,木曜3限,木曜6限,木曜7限,金曜3限,金曜4限,金曜5限,金曜6限,金曜7限,土曜1限,土曜2限,土曜3限,土曜4限,土曜5限,土曜6限,土曜7限",
-                        "Edit" }
-        };
-
     }
 
     public static void main(String[] args) {
